9a209edaecbb0b73a151273a90e88771763c9378
Aymane <aymane-chaoui.adib@student-cs.fr>
2025-05-16 23:27:39 +0200

    initialisation of packages

--- cs.fr.groupB.myFoodora/src/users/Customer.java
+++ cs.fr.groupB.myFoodora/src/users/Customer.java
@@ -1,10 +1,18 @@
 package users;
+
+import java.util.*;
+
 import fidelity.*;
 
 public class Customer {
 
-    Location adress;
-    String phoneNumber;
-    FidelityCard fidelityCard;
+    private static Set<String> emailsUsed = new HashSet<>();
     
+    private Location adress;
+    private String phoneNumber;
+    private FidelityCard fidelityCard;
+    private boolean notificationsConsent;
+    private String email;
+    
+
 }
