fcb0ecd90c9eee37358070d9a9cbbc061cedafe0
Aymane <aymane-chaoui.adib@student-cs.fr>
2025-05-18 17:40:09 +0200

    Finishing Fidelity Cards

--- cs.fr.groupB.myFoodora/src/fidelity/FidelityCardFactory.java
+++ cs.fr.groupB.myFoodora/src/fidelity/FidelityCardFactory.java
@@ -1,5 +1,18 @@
 package fidelity;
 
-public class FidelityCardFactory {
+import users.*;
 
+public class FidelityCardFactory {
+    public static FidelityCard createFidelityCard(String type, Customer owner) {
+        switch (type.toLowerCase()) {
+            case "basiccard":
+                return new BasicCard(owner);
+            case "pointcard":
+                return new PointCard(owner);
+            case "lotterycard":
+                return new LotteryCard(owner);
+            default:
+                throw new IllegalArgumentException("Unknown Fidelity Card Type: " + type);
+        }
+    }
 }
--- cs.fr.groupB.myFoodora/src/users/CourierComparator.java
+++ cs.fr.groupB.myFoodora/src/users/CourierComparator.java
@@ -6,6 +6,6 @@ import java.util.Comparator;
 public class CourierComparator implements Comparator<Courier> {
 	@Override
 	public int compare(Courier c1, Courier c2) {
-		return c2.getDeliveryCounter()-c2.getDeliveryCounter();
+		return c2.getDeliveryCounter()-c1.getDeliveryCounter();
 	}
 }
