2a3798df6490d72eae47a935584d09bb793c00b9
Alisson Bonatto <alissonbonatto@hotmail.com>
2025-06-01 17:25:01 +0200

    JavaDoc comments generated

--- cs.fr.groupB.myFoodora/src/cli/BasicScenarioGenerator.java
+++ cs.fr.groupB.myFoodora/src/cli/BasicScenarioGenerator.java
@@ -30,6 +30,12 @@ import user.Restaurant;
 import user.User;
 import user.UserFactory;
 
+/**
+ * Generates a basic scenario with a predefined set of users (customers, restaurants, couriers, manager)
+ * and associated data like menus and dishes for testing or demonstration purposes.
+ * 
+ * @author Alisson Bonatto
+ */
 public class BasicScenarioGenerator{
 
 	private Set<Customer> customers;             // Set of all costumers
@@ -38,7 +44,10 @@ public class BasicScenarioGenerator{
     private Set<Courier> couriers;               // Set of all couriers
     private Map<String, User> userMap;           // HashMap <username, user>
     
-    
+    /**
+     * Constructs a BasicScenarioGenerator and initializes the sets for customers,
+     * restaurants, managers, couriers, and the user map.
+     */
 	public BasicScenarioGenerator() {
 		this.customers = new HashSet<Customer>();
 		this.restaurants = new HashSet<Restaurant>();
@@ -47,6 +56,22 @@ public class BasicScenarioGenerator{
 		this.userMap = new HashMap<String, User>();
 	}
 	
+	/**
+     * Creates a specified number of random users (restaurants, customers, couriers)
+     * and a single manager. These users are added to their respective sets and
+     * to the global user map.
+     *
+     * @param restaurantQuantity The number of random restaurants to create
+     * @param customerQuantity   The number of random customers to create
+     * @param courierQuantity    The number of random couriers to create
+     * @throws BadUserCreationException        If there is an error during user creation
+     * @throws BadNumberOfArgumentsException   If an incorrect number of arguments is provided for user/dish/meal creation
+     * @throws BadDishTypeCreationException    If there is an error during dish type creation
+     * @throws BadArgumentTypeException        If an argument of an incorrect type is provided
+     * @throws BadMealTypeCreationException    If there is an error during meal type creation
+     * @throws UnrecognizedDishException       If a dish type is not recognized
+     * @throws BadMealFormulaException         If there is an error in the meal formula
+     */
 	public void createRandomUsers(int restaurantQuantity, int customerQuantity, int courierQuantity) throws BadUserCreationException,
 				BadNumberOfArgumentsException, BadDishTypeCreationException, BadArgumentTypeException, BadMealTypeCreationException,
 				UnrecognizedDishException, BadMealFormulaException {
@@ -61,22 +86,52 @@ public class BasicScenarioGenerator{
 		this.managers.add(manager);
 	}
 	
+	/**
+     * Returns the set of customers created by this generator.
+     *
+     * @return customers the set of customers
+     */
 	public Set<Customer> getCreatedCustomers(){
 		return this.customers;
 	}
 	
+	/**
+     * Returns the set of restaurants created by this generator.
+     *
+     * @return restaurants the set of restaurants
+     */
 	public Set<Restaurant> getCreatedRestaurants(){
 		return this.restaurants;
 	}
 	
+	/**
+     * Returns the set of couriers created by this generator.
+     *
+     * @return couriers the set of couriers
+     */
 	public Set<Courier> getCreatedCouriers(){
 		return this.couriers;
 	}
 	
+	/**
+     * Returns the map of all users created by this generator, keyed by their username.
+     *
+     * @return userMap the user map mapping the username with the user
+     */
 	public Map<String, User> getCreatedUserMap(){
 		return this.userMap;
 	}
 
+	/**
+     * Creates a specified quantity of random customers.
+     * Each customer is given a unique generated name, surname, username, password,
+     * phone number, email, and random coordinates.
+     * They are added to the main user map.
+     *
+     * @param quantity the number of customers to create
+     * @return a HashSet containing the created {@link Customer} objects
+     * @throws BadUserCreationException If an error occurs during customer creation
+     */
 	private HashSet<Customer> createRandomCustomers(int quantity) throws BadUserCreationException {
 		UserFactory userFactory = new UserFactory();
 		HashSet<Customer> customers = new HashSet<Customer>();
@@ -101,6 +156,16 @@ public class BasicScenarioGenerator{
 		return customers;
 	}
 	
+	/**
+     * Creates a specified quantity of random couriers.
+     * Each courier is given a unique generated name, surname, username, password,
+     * phone number, and random coordinates.
+     * They are added to the main user map.
+     *
+     * @param quantity The number of couriers to create
+     * @return a HashSet containing the created {@link Courier} objects
+     * @throws BadUserCreationException If an error occurs during courier creation
+     */
 	private HashSet<Courier> createRandomCouriers(int quantity) throws BadUserCreationException{
 		UserFactory userFactory = new UserFactory();
 		HashSet<Courier> couriers = new HashSet<Courier>();
@@ -126,6 +191,22 @@ public class BasicScenarioGenerator{
 		return couriers;
 	}
 	
+	/**
+     * Creates a specified quantity of random restaurants.
+     * Each restaurant is given a unique generated name, username, password, and
+     * random coordinates. A random menu is also generated for each restaurant.
+     * They are added to the main user map.
+     *
+     * @param quantity The number of restaurants to create.
+     * @return a HashSet containing the created {@link Restaurant} objects.
+     * @throws BadUserCreationException        If an error occurs during restaurant creation
+     * @throws BadNumberOfArgumentsException   If an incorrect number of arguments is provided for dish/meal creation within the menu
+     * @throws BadDishTypeCreationException    If there is an error during dish type creation for the menu
+     * @throws BadArgumentTypeException        If an argument of an incorrect type is provided for the menu
+     * @throws BadMealTypeCreationException    If there is an error during meal type creation for the menu
+     * @throws UnrecognizedDishException       If a dish type is not recognized for the menu
+     * @throws BadMealFormulaException         If there is an error in the meal formula for the menu
+     */
 	private HashSet<Restaurant> createRandomRestaurants(int quantity) throws BadUserCreationException, BadNumberOfArgumentsException,
 				BadDishTypeCreationException, BadArgumentTypeException, BadMealTypeCreationException, UnrecognizedDishException, BadMealFormulaException{
 		UserFactory userFactory = new UserFactory();
@@ -159,6 +240,22 @@ public class BasicScenarioGenerator{
 		
 	}
 	
+	/**
+     * Creates a random menu for a restaurant.
+     * The menu will contain a specified number of meals, composed of randomly
+     * generated starters, main dishes, and desserts.
+     * At least one meal will be a "meal of the week" with a discount.
+     *
+     * @param quantityMeals  The number of meals to include in the menu.
+     * @param restaurantName The name of the restaurant, used for generating unique dish/meal names.
+     * @return a randomly generated {@link Menu} object.
+     * @throws BadNumberOfArgumentsException If an incorrect number of arguments is provided for dish/meal creation
+     * @throws BadDishTypeCreationException    If there is an error during dish type creation
+     * @throws BadArgumentTypeException        If an argument of an incorrect type is provided
+     * @throws BadMealTypeCreationException    If there is an error during meal type creation
+     * @throws UnrecognizedDishException       If a dish type is not recognized
+     * @throws BadMealFormulaException         If there is an error in the meal formula
+     */
 	private Menu createRandomMenu(int quantityMeals, String restaurantName) throws BadNumberOfArgumentsException, BadDishTypeCreationException,
 				BadArgumentTypeException, BadMealTypeCreationException, UnrecognizedDishException, BadMealFormulaException{
 		// Useful variables
@@ -224,6 +321,18 @@ public class BasicScenarioGenerator{
 		return menu;
 	}
 	
+	/**
+     * Creates a specified quantity of random dishes.
+     * Dishes can be starters, main dishes, or desserts, with random names, prices,
+     * and gluten-free/vegetarian status. Ensures at least one of each dish type is created.
+     *
+     * @param quantity       The total number of random dishes to create
+     * @param restaurantName The name of the restaurant, used for generating unique dish names
+     * @return A HashSet containing the created {@link Dish} objects
+     * @throws BadNumberOfArgumentsException If an incorrect number of arguments is provided for dish creation
+     * @throws BadDishTypeCreationException    If there is an error during dish type creation
+     * @throws BadArgumentTypeException        If an argument of an incorrect type is provided for dish creation
+     */
 	private HashSet<Dish> createRandomDishes(int quantity, String restaurantName) throws BadNumberOfArgumentsException,
 				BadDishTypeCreationException, BadArgumentTypeException{
 		DishFactory dishFactory = new DishFactory();
